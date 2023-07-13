53b0e6e668dddccf0df25928c646307b65b5a0ff
Rafael Brenes <rbrenesr@gmail.com>
2023-07-13 07:49:09 -0600

    CalednarApp Cap.414 Creación de un nuevo usuario {RB}

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -17,7 +17,7 @@ const registerFormFields = {
 
 export const LoginPage = () => {
 
-    const { startLogin, errorMessage } = useAuthStore();
+    const { startLogin, startRegister, errorMessage } = useAuthStore();
     const { loginEmail, loginPassword, onInputChange: onLoginInputChange } = useForm(loginFormFields);
     const { registerName, registerEmail, registerPassword, registerPassword2, onInputChange: onRegisterInputChange } = useForm(registerFormFields);
 
@@ -28,7 +28,13 @@ export const LoginPage = () => {
 
     const registerSubmit = (event) => {
         event.preventDefault();
-        console.log({ registerName, registerEmail, registerPassword, registerPassword2 });
+
+        if (registerPassword != registerPassword2) {
+            Swal.fire('Error de registro', 'Las contraseñas deben ser iguales', 'error');
+            return;
+        }
+
+        startRegister({ name: registerName, email: registerEmail, password: registerPassword });
     }
 
 
--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -32,9 +32,60 @@ export const useAuthStore = () => {
     }
 
 
+    const startRegister = async ({ name, email, password }) => {
+        dispatch(onChecking());
+
+        try {
+
+            const { data } = await calendarApi.post('/auth/new', { name, email, password });
+            localStorage.setItem('token', data.token);
+            localStorage.setItem('token-init-date', new Date().getTime());
+            dispatch(onLogin({ name: data.name, uid: data.uid }));
+
+        } catch (error) {
+
+
+console.log(error.response.data);
+
+            const message =
+                error.response.data?.msg ||
+                Object.values(error.response.data?.errores.errors)
+                    .map((x) => x.msg)
+                    .join();
+
+            dispatch(onLogout(message));
+
+            setTimeout(() => {
+                dispatch(clearErrorMessages());
+            }, 10);
+
+/*
+            const { response } = error;
+
+            if (response.data.errores) {
+
+                dispatch(onLogout(response.data.errores.errors[0].msg));
+
+                setTimeout(() => {
+                    dispatch(clearErrorMessages());
+                }, 10);
+            } else if (response.data.msg) {
+
+                dispatch(onLogout(response.data.msg));
+
+                setTimeout(() => {
+                    dispatch(clearErrorMessages());
+                }, 10);
+            }
+
+            */
+        }
+    }
+
     return {
         status, user, errorMessage,
 
         startLogin,
+        startRegister,
     }
 }
