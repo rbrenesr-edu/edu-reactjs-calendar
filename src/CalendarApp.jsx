64187784787467080b54a5c5e574cbdce0e36a55
Rafael Brenes <rbrenesr@gmail.com>
2023-07-14 00:31:57 -0600

    Production {RB}

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -1,14 +1,20 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { AppRouter } from "./router";
 import { Provider } from "react-redux";
 import { store } from "./store";
 
 export const CalendarApp = () => {
   return (
-    <Provider store={ store } >
+    <Provider store={store} >
+
+
       <BrowserRouter>
         <AppRouter />
       </BrowserRouter>
+      {/** De esta forma, con el HashRouter, contorlamos el despliegue y errores de rura #/auth/login al refrescar la pagina login */}
+      {/* <HashRouter>
+        <AppRouter />
+      </HashRouter> */}
     </Provider>
   );
 };
